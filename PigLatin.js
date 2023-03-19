af3f104a13617ec8970f82974e0b6e857de88321
Sultan Delen <99362897+sultandlen@users.noreply.github.com>
2023-03-19 13:53:48 +0300

    Complete PigLatin

--- PigLatin.js
+++ PigLatin.js
@@ -1,10 +1,25 @@
-function pigIt(str) {
+/*function pigIt(str) {
   let arr = str.split(" ");
   let newStr = "";
   arr.forEach((element) => {
-    newStr += element.slice(1) + element[0] + "ay ";
+    if (!/^[a-zA-Z]/.test(element)) {
+      newStr += element;
+    } else {
+      newStr += element.slice(1) + element[0] + "ay ";
+    }
   });
   return newStr;
+}*/
+
+function pigIt(str) {
+  let arr = str.split(" ");
+  return arr.reduce((acc, element) => {
+    if (!/^[a-zA-Z]/.test(element)) {
+      return acc + element;
+    } else {
+      return acc + (element.slice(1) + element[0] + "ay ");
+    }
+  }, "");
 }
 
-console.log(pigIt("Pig latin is cool"));
+console.log(pigIt("Pig latin is cool ?"));
