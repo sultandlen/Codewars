ddcf7e219c4f80524f4d1031d3071784d7642886
Sultan Delen <99362897+sultandlen@users.noreply.github.com>
2023-05-29 20:32:00 +0300

    Create weightForWeight.js

---
+++ weightForWeight.js
@@ -0,0 +1,29 @@
+function orderWeight(str) {
+  if (str === "") return "";
+  let result = [];
+  let weights = str.split(" ");
+  for (let val of weights) {
+    result.push({
+      val,
+      sum: sumOfDigits(val),
+    });
+  }
+  console.log(result);
+  return result
+    .sort(compare)
+    .map((x) => x.val)
+    .join(" ");
+}
+
+function compare(a, b) {
+  if (a.sum !== b.sum) {
+    return a.sum - b.sum;
+  }
+  return a.val.localeCompare(b.val);
+}
+
+function sumOfDigits(num) {
+  return num.split("").reduce((acc, digit) => acc + Number(digit), 0);
+}
+
+console.log(orderWeight("100 21 25 30"));
