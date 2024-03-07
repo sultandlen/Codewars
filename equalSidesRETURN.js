dd0c100cfbeafea661939831a3e41da00e7c6ebc
Sultan Delen <99362897+sultandlen@users.noreply.github.com>
2024-03-07 15:59:47 +0300

    Update equalSidesRETURN.js

--- equalSidesRETURN.js
+++ equalSidesRETURN.js
@@ -1,17 +1,18 @@
 function findEvenIndex(arr) {
-  const sum = arr.reduce((partialSum, a) => partialSum + a, 0);
-  let leftSum = (rightSum = index = 0);
-  while (leftSum <= sum / 2) {
-    leftSum += arr[index];
-    index++;
+  let leftSum = 0;
+  let index = -1;
+  for (let i = 0; i < arr.length; i++) {
+    leftSum += arr[i];
+    let rightSum = 0;
+    for (let j = i + 1; j < arr.length; j++) {
+      rightSum += arr[j];
+    }
+    if (leftSum - arr[i] === rightSum) {
+      index = i;
+      break;
+    }
   }
-
-  for (let i = index - 1; i < arr.length; i++) {
-    rightSum += arr[i];
-  }
-  if (rightSum === leftSum) {
-    return index - 1;
-  }
-  return -1;
+  return index;
 }
-console.log(findEvenIndex([10, -80, 10, 10, 15, 35, 20]));
+
+console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1]));
